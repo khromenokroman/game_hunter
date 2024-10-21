70297291118c7de68e4a256b4ea186ed208c46da
khromenokroman <roma55592@yandex.ru>
2024-10-21 20:04:12 +0200

    Добавлена анимация для цели

--- src/game.cpp
+++ src/game.cpp
@@ -5,25 +5,53 @@
 #include "game.hpp"
 
 hunter::game::Game::Game()
-    : m_window{SDL_CreateWindow("test_window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, hunter::CONSTANTS::START_WINDOW_WIDTH,
+    : m_window{SDL_CreateWindow("hunter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, hunter::CONSTANTS::START_WINDOW_WIDTH,
                                 hunter::CONSTANTS::START_WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE),
                SDL_DestroyWindow},
       m_renderer{SDL_CreateRenderer(m_window.get(), -1, SDL_RENDERER_ACCELERATED), SDL_DestroyRenderer} {
     m_background.load_images(m_renderer.get(), "../images/zone1.jpg", nullptr);
     m_background.load_images(m_renderer.get(), "../images/aim.png", &m_surface_aim);
+    m_background.load_images(m_renderer.get(), "../images/target.png", &m_surface_target);
 }
 void hunter::game::Game::run() {
     std::cout << "Start main loop game" << std::endl;
 
-    m_surface_aim_dst = {100, 100, m_surface_aim.w, m_surface_aim.h};
+    m_surface_aim_dst = {100, 200, m_surface_aim.w, m_surface_aim.h};
+    m_surface_target = {300, 20, 550, 370};
+    m_surface_target_dst = {100, 100, 150, 100};
+
+    int frame = 1;
+    int frame_count = 4;
+    int cur_frame_time = 0;
+    int max_frame_time = 300;
+    int last_time = static_cast<int>(SDL_GetTicks());
+    int new_time = 0;
+    int dt = 0;
+
     SDL_Event events;
     while (m_is_running) {
         get_os_event(events);
+
+        new_time = static_cast<int>(SDL_GetTicks());
+        dt = new_time - last_time;
+        last_time = new_time;
+        cur_frame_time += dt;
+        if (cur_frame_time > max_frame_time) {
+            cur_frame_time -= max_frame_time;
+            frame += 1;
+            if (frame > frame_count) {
+                frame = 1;
+                m_surface_target.x = 300;
+            } else {
+                m_surface_target.x += m_surface_target.w;
+            }
+        }
         SDL_RenderClear(m_renderer.get());
         SDL_RenderCopy(m_renderer.get(), m_background.textures[0].get(), nullptr, nullptr);
+        SDL_RenderCopy(m_renderer.get(), m_background.textures[2].get(), &m_surface_target, &m_surface_target_dst);
         SDL_RenderCopy(m_renderer.get(), m_background.textures[1].get(), &m_surface_aim, &m_surface_aim_dst);
         SDL_RenderPresent(m_renderer.get());
-        SDL_Delay(100);
+        SDL_Delay(1000 / 60);
     }
 }
 void hunter::game::Game::get_os_event(SDL_Event& event) {
--- src/game.hpp
+++ src/game.hpp
@@ -13,6 +13,8 @@ struct Game {
     hunter::background::Background m_background;                        // сервис загрузка картинок
     SDL_Rect m_surface_aim{0, 0, 0, 0};                                 // полигон для прицела
     SDL_Rect m_surface_aim_dst{0, 0, 0, 0};                             // полигон где будет размещаться прицел
+    SDL_Rect m_surface_target{0, 0, 0, 0};                              // полигон для мишени
+    SDL_Rect m_surface_target_dst{0, 0, 0, 0};                          // полигон где будет размещаться мишень
     std::unique_ptr<SDL_Window, void (*)(SDL_Window *)> m_window;       // окно
     std::unique_ptr<SDL_Renderer, void (*)(SDL_Renderer *)> m_renderer; // рисовальщик
     bool m_is_running{true};                                            // флаг работы главного цикла игры
